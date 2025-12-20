e4ce3897a39a223dee44e7abd6fb319aae21ec45
Mr. Radd <conrad.horton@gmail.com>
2025-12-19 21:28:50 -0800

    Comment update.

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,10 +1,9 @@
-
 export interface Task {
     id?: string; //UUID This will be undefined if we are working with a new Task.
     title: string;
     description?: string;
-    due_date?: Date;
-    created_date?: Date; //This will be undefined if we are working with a new Task.
+    due_date?: Date; //UTC
+    created_date?: Date; //UTC. This will be undefined if we are working with a new Task.
     status: number; // 0 = Incomplete, 1 = Complete
     category_id?: string; //UUID
 }
